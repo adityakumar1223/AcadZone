408251094f774d39ed127d73bbe0bd02d598770e
Aditya Kumar <adityakumarak1223@gmail.com>
2025-07-31 23:21:04 +0530

    First Commit

---
+++ backEnd/AcadZone/src/main/java/com/AcadZone/AcadZoneApplication.java
@@ -0,0 +1,13 @@
+package com.AcadZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadZoneApplication.class, args);
+	}
+
+}
